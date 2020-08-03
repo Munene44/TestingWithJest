d9ff8fde3f1bc8ed897e8dff83e965e5be67bc34
nenekelvin <karvinmitchar@gmail.com>
2020-08-03 12:40:10 +0300

    first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,9 @@
-App.test.js
+it("renders without crashing", () => {
+  shallow(<App />);
+});
+
+it("renders Account header", () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h1>Display Active Users Account Details</h1>;
+  expect(wrapper.contains(welcome)).toEqual(true);
+});
